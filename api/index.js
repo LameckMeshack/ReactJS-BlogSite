861a47717290acb43c7796b2e1ddeb6a985eb6ad
RhinolamerSonwar <rhinolamer@gmail.com>
2021-08-02 15:37:06 +0300

    first commit

---
+++ api/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const userRoute = require("./routes/users");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+const path = require("path");
+
+dotenv.config();
+app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
+//connecting to db
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+    // cb(null, "hello.jpeg"); when testing in post in postman
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
+app.listen("5000", () => {
+  console.log("Th backend is running");
+});
